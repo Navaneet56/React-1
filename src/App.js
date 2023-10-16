352291feae3a90b548c1bdc99c555889986690d1
Navaneeth <naninavaneeth56@gmail.com>
2023-10-16 22:54:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React,{ useState } from 'react';
-import WordCounter from './WordCounter';
+import React,{useState} from 'react'
 import './App.css';
-
-function App() {
-  const [text, setText] = useState('');
-
-  const textchanger = (e) => {
-    setText(e.target.value);
-  };
-
+export default function Nage() {
+    const [input,setInput] = useState("");
+    const [result,setResult] = useState(0);
+    const handler = e =>{
+        setInput(e.target.value);
+    }
+    const handleclear = e =>{
+        setInput('');
+        setResult();
+    }
+    
   return (
-    <div className="App">
-      <h1>Responsive Paragraph Word Counter</h1>
-      <textarea
-        rows="8"
-        placeholder="Enter text here"
-        value={text}
-        onChange={textchanger}
-      ></textarea>
-      <WordCounter text={text} />
-    </div>
-  );
-}
+    <div>
+      <div className='con'>
+        <input type='text' className='inputval' value={input} name='input' onChange={handler}></input>
+        <div className='res'>
+        {result}
+        </div>
+        
+        <div className='btncon'>
+        
+            <div className='num'>
+                <div className='num1'>
+                    <button className='btns' id='bt9' onClick={() => setInput(input+'9')}>9</button>
+                    <button className='btns' id='bt8' onClick={() => setInput(input+'8')}>8</button>
+                    <button className='btns' id='bt7' onClick={() => setInput(input+'7')}>7</button>
+                </div>
+                <div className='num1'>
+                    <button className='btns' id='bt6' onClick={() => setInput(input+'6')}>6</button>
+                    <button className='btns' id='bt5' onClick={() => setInput(input+'5')}>5</button>
+                    <button className='btns' id='bt4' onClick={() => setInput(input+'4')}>4</button>
+                </div>
+                <div className='num1'>
+                    <button className='btns' id='bt3' onClick={() => setInput(input+'3')}>3</button>
+                    <button className='btns' id='bt2' onClick={() => setInput(input+'2')}>2</button>
+                    <button className='btns' id='bt1' onClick={() => setInput(input+'1')}>1</button>
+                </div>
+                <div className='num1'>
+                    <button className='btns' id='bt0' onClick={() => setInput(input+'0')}>0</button>
+                    <button className='btns' id='bt.' onClick={() => setInput(input+'.')}>.</button>
+                    <button className='btns' id='bt=' onClick={() => setResult(eval(input))}>=</button>
+                </div>
+            </div>
 
-export default App;
+            <div className='exp'>
+                <button className='btns' id='btb' onClick={handleclear}>Clear</button>
+                <button className='btns' id='btd' onClick={() => setInput(input+'/')}>/</button>
+                <button className='btns' id='bt*' onClick={() => setInput(input+'*')}>*</button>
+                <button className='btns' id='bt-' onClick={() => setInput(input+'-')}>-</button>
+                <button className='btns' id='bt+' onClick={() => setInput(input+'+')}>+</button>
+                
+            </div>
+
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
